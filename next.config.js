f8217a9765af218319670334e6f4be318446ccb5
Ali <alim.9171@gmail.com>
2022-02-07 15:23:17 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['localhost', 'cdn0.iconfinder.com' , 'tsl.designer-dev.com'],
-    eslint: { ignoreDuringBuilds: true },
   },
+  eslint: { ignoreDuringBuilds: true },
 }
