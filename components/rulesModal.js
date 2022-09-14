c2936f4952244e0f2cb59fc621ee4cc743a958d2
Ali-Muhammad-123 <Alim.9171@gmail.com>
2022-09-14 18:29:03 +0500

    added

--- components/rulesModal.js
+++ components/rulesModal.js
@@ -1,45 +1,48 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import StyledTextField from '../styledComponents/styledTextField';
-import StyledButton from '../styledComponents/styledButton';
-import { Grid } from '@mui/material';
-import { useDispatch  , useSelector} from 'react-redux';
-import { setRules } from '../redux/addHotel';
-import { setRoomRules } from '../redux/addRoom';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import StyledTextField from "../styledComponents/styledTextField";
+import StyledButton from "../styledComponents/styledButton";
+import { Grid } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { setRules } from "../redux/addHotel";
+import { setRoomRules } from "../redux/addRoom";
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '320px',
-  bgcolor: 'background.main',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "100%",
+  minWidth: "320px",
+  maxWidth: "500px",
+  bgcolor: "background.main",
   boxShadow: 24,
   p: 4,
-  borderRadius:'10px'
+  borderRadius: "10px",
 };
 
 export default function RulesModal(props) {
   const [open, setOpen] = React.useState(false);
-  const [name , setName] = React.useState('');
-  const [description , setDescription] = React.useState('');
-  const rules = useSelector(state => state.addHotel.rules)
-  const roomRules = useSelector(state => state.addRoom.roomRules)
+  const [name, setName] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const rules = useSelector((state) => state.addHotel.rules);
+  const roomRules = useSelector((state) => state.addRoom.roomRules);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  function addrule(){
-    handleClose()
-    if(props.hotel === true)
-    {var copy_rules = rules.map((el)=>(el))
-    copy_rules.push({service_name:name , service_description:description})
-    dispatch(setRules(copy_rules))}
-    else{
-    var copy_rules = roomRules.map((el)=>(el))
-    copy_rules.push({service_name:name , service_description:description})
-    dispatch(setRoomRules(copy_rules))}
+  function addrule() {
+    handleClose();
+    if (props.hotel === true) {
+      var copy_rules = rules.map((el) => el);
+      copy_rules.push({ service_name: name, service_description: description });
+      dispatch(setRules(copy_rules));
+    } else {
+      var copy_rules = roomRules.map((el) => el);
+      copy_rules.push({ service_name: name, service_description: description });
+      dispatch(setRoomRules(copy_rules));
+    }
   }
 
   return (
@@ -52,23 +55,46 @@ export default function RulesModal(props) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-        <Grid container spacing={2}>
-        <Grid container item spacing={2}>
-            <Grid item xs={3} >
-          <Typography sx={{color:'primary.main'}} variant='p'>Rule Name</Typography></Grid>
-          <Grid item xs={8}>
-          <StyledTextField onChange={(e) => setName(e.target.value)} fullWidth/></Grid>
-          </Grid>
-          <Grid container item spacing={2}>
+          <Grid container spacing={2} fullWidth>
+            <Grid container item spacing={2}>
               <Grid item xs={3}>
-          <Typography sx={{color:'primary.main'}} variant='p'>Rule Description</Typography></Grid>
-          <Grid item xs={8}>
-          <StyledTextField onChange={(e) => setDescription(e.target.value)} fullWidth /></Grid>
+                <Typography sx={{ color: "primary.main" }} variant="p">
+                  Rule Name
+                </Typography>
+              </Grid>
+              <Grid item xs={8}>
+                <StyledTextField
+                  onChange={(e) => setName(e.target.value)}
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+            <Grid container item spacing={2}>
+              <Grid item xs={3}>
+                <Typography sx={{ color: "primary.main" }} variant="p">
+                  Rule Description
+                </Typography>
+              </Grid>
+              <Grid item xs={8}>
+                <StyledTextField
+                  onChange={(e) => setDescription(e.target.value)}
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+            <Grid
+              sx={{ margin: "10px 0px 0px 0px" }}
+              container
+              justifyContent="space-around"
+              spacing={2}
+            >
+              <Grid item>
+                <StyledButton onClick={() => addrule()}>
+                  Add Question
+                </StyledButton>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid sx={{margin:'10px 0px 0px 0px'}} container justifyContent='space-around' spacing={2}>
-              <Grid item><StyledButton onClick={() => addrule()}>Add Question</StyledButton></Grid>
-          </Grid>
-        </Grid>
         </Box>
       </Modal>
     </div>
--- pages/vendor/addroom/index.js
+++ pages/vendor/addroom/index.js
@@ -1,54 +1,91 @@
 import React from "react";
-import {  Grid , Typography , TextField , InputAdornment } from "@mui/material";
+import { Grid, Typography, TextField, InputAdornment } from "@mui/material";
 import HotelCard from "../../../components/hotelCard";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 import StyledContainer from "../../../styledComponents/styledContainer";
 import VendorLayout from "../../../components/vendorLayout";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { API_URL } from "../../../config";
-import useSWR from 'swr'
+import useSWR from "swr";
 import { useRouter } from "next/router";
 
-const fetch2 = (user,token) =>  axios({
-            method: 'GET',
-            url: `${API_URL}/hotels?users_permissions_user.id=${user.id}`,
-            headers:{
-              Accept: 'application/json',
-              'Authorization': `Bearer ${token}`
-            }
-          }).then(res => res.data)
+const fetch2 = (user, token) =>
+  axios({
+    method: "GET",
+    url: `${API_URL}/hotels?users_permissions_user.id=${user.id}`,
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => res.data);
 
 function Index(props) {
-    const router = useRouter() 
-    const token = useSelector(state => state.user.token)
-    const user = useSelector(state => state.user.user)
-    const { data, error } = useSWR([user , token] ,fetch2)
-    return (
-        <StyledContainer square >
-            <Grid container spacing={2}>
-            <Grid  item xs={12}  sx={{textAlign:'left'}}>
-                <Typography variant='h6'  color='primary.main'>All Hotels</Typography>
-                </Grid>
-            <Grid  item xs={12}  sx={{textAlign:'left'}}>
-                <TextField InputLabelProps={{shrink: false}} label='' sx={{'& .MuiOutlinedInput-notchedOutline':{borderColor:'primary.main'} ,'& .MuiSvgIcon-root':{color:'#FFF'} ,'& .MuiOutlinedInput-root' :{paddingRight:'0px'} , '& .MuiInputAdornment-outlined' :{padding:'27px 10px' , borderRadius:'4px' ,backgroundColor:'button.main'} , '& .MuiInputAdornment-root':{backgroundColor:''} }} placeholder="Search by Hotel Name"   fullWidth variant='outlined' InputProps={{endAdornment: <InputAdornment><SearchIcon style={{fontSize:'2.5rem'}}/></InputAdornment>}}/>
-                </Grid>
-                {data && data[0].hotelname? data.map((element)=>
-                (<Grid item xs={12} sm={6} md={4} sx={{textAlign:'center'}}>
-                    {console.log(data)}
-                <HotelCard name={element.hotelname} icons={[...element.amenities , ...element.facilities]} startingPrice={element.starting_price} images={element.images} buttonText={'Add room in this hotel'} clickFunction={() => router.push({pathname:`/vendor/addroom/${element.id}`})} /> </Grid>)):''}
-                </Grid>
-        </StyledContainer>
-    );
+  const router = useRouter();
+  const token = useSelector((state) => state.user.token);
+  const user = useSelector((state) => state.user.user);
+  const { data, error } = useSWR([user, token], fetch2);
+  return (
+    <StyledContainer square>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sx={{ textAlign: "left" }}>
+          <Typography variant="h6" color="primary.main">
+            All Hotels
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sx={{ textAlign: "left" }}>
+          <TextField
+            InputLabelProps={{ shrink: false }}
+            label=""
+            sx={{
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "primary.main",
+              },
+              "& .MuiSvgIcon-root": { color: "#FFF" },
+              "& .MuiOutlinedInput-root": { paddingRight: "0px" },
+              "& .MuiInputAdornment-outlined": {
+                padding: "27px 10px",
+                borderRadius: "4px",
+                backgroundColor: "button.main",
+              },
+              "& .MuiInputAdornment-root": { backgroundColor: "" },
+            }}
+            placeholder="Search by Hotel Name"
+            fullWidth
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <SearchIcon style={{ fontSize: "2.5rem" }} />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+        {data && data[0].hotelname
+          ? data.map((element) => (
+              <Grid item xs={12} sm={6} md={4} sx={{ textAlign: "center" }}>
+                {console.log(data)}
+                <HotelCard
+                  name={element.hotelname}
+                  icons={[...element.amenities, ...element.facilities]}
+                  startingPrice={element.starting_price}
+                  images={element.images}
+                  buttonText={"Add room in this hotel"}
+                  clickFunction={() =>
+                    router.push({ pathname: `/vendor/addroom/${element.id}` })
+                  }
+                />{" "}
+              </Grid>
+            ))
+          : ""}
+      </Grid>
+    </StyledContainer>
+  );
 }
 
 Index.getLayout = function getLayout(Index) {
-    return (
-      <VendorLayout>
-        {Index}
-      </VendorLayout>
-    )
-  }
+  return <VendorLayout>{Index}</VendorLayout>;
+};
 
-
-export default Index
+export default Index;
